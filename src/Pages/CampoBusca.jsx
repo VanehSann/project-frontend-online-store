1ebb6993bacd00d6fd23b2aa1ef0dceb1f742b57
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-10 16:16:41 -0300

    Criada a pasta Pages. Resolvido requisito 2

---
+++ src/Pages/CampoBusca.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CampoBusca extends Component {
+  render() {
+    return (
+      <div>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+      </div>
+    );
+  }
+}
+export default CampoBusca;
