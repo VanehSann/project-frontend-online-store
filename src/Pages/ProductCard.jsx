16a62b9cec1aaf7cd652fa492a58014d79d83bb5
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-10 19:20:34 -0300

    Criada a componentDidMount

--- src/Pages/ProductCard.jsx
+++ src/Pages/ProductCard.jsx
@@ -10,13 +10,20 @@ class ProductCard extends Component {
     console.log(this.props);
   }
 
+  componentDidMount() {
+    const { produtos } = this.props;
+    this.setState({
+      produtos,
+    });
+  }
+
   render() {
     const { encontrou, produtos } = this.state;
     return (
       encontrou ? (
         <h3>Nenhum produto foi encontrado</h3>) : (
         produtos.map((produto) => (
-          <div data-testid="product">
+          <div data-testid="product" key={ produto.title }>
             <h2>{ produto.title }</h2>
             <img src={ produto.thumbnail } alt={ produto.title } />
             <h3>{ produto.price }</h3>
