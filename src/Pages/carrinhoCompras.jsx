e6ecc70b612fb2862e23ab3d13e10c4560639ac8
Roger Consul <rogerconsul@yahoo.com.br>
2022-03-15 15:26:47 -0300

    Tentando resolver local storage

--- src/Pages/carrinhoCompras.jsx
+++ src/Pages/carrinhoCompras.jsx
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 class carrinhoCompras extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: '',
+      price: '',
+      thumb: '',
+      title: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      id: localStorage.getItem('produtoID'),
+      price: localStorage.getItem('produtoPrice'),
+      thumb: localStorage.getItem('produtoThumb'),
+      title: localStorage.getItem('produtoTitle'),
+    });
+  }
+
   render() {
-    return (<p>asdas</p>);
+    const { id, price, thumb, title } = this.state;
+    return (
+      <div>
+        <h3>{ title }</h3>
+        <img src={ thumb } alt={ title } />
+        <h5>{ `R$${price}` }</h5>
+      </div>
+    );
   }
 }
 
