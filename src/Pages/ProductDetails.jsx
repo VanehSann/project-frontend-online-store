8de347eb9e72554640d3af4dfbac28090659da5d
VanehSann <vanessacbsantos22@gmail.com>
2022-09-12 01:43:40 -0300

    refactor: remove trybe files

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -8,7 +8,13 @@ class ProductDetails extends Component {
     super();
     this.state = {
       produto: '',
+      email: '',
+      textarea: '',
+      nota: '',
+      avaliacoes: [],
+      salvaavaliacoes: [],
     };
+    this.onChangeInput = this.onChangeInput.bind(this);
   }
 
   async componentDidMount() {
@@ -19,15 +25,55 @@ class ProductDetails extends Component {
     });
   }
 
+  onChangeInput({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  click = () => {
+    const { email, nota, textarea, avaliacoes } = this.state;
+    this.setState({
+      avaliacoes: [email, nota, textarea],
+
+    });
+    this.setState((prev) => ({
+      salvaavaliacoes: [...prev.salvaavaliacoes, email, nota, textarea],
+      email: '',
+      textarea: '',
+
+    }));
+  }
+
   render() {
-    const { produto } = this.state;
+    const { produto, email, nota, textarea, avaliacoes } = this.state;
+    console.log(avaliacoes);
     return (
-      <div>
-        <h3 data-testid="product-detail-name">{ produto.title }</h3>
-        <img src={ produto.thumbnail } alt={ produto.title } />
-        <h4>{`R$ ${produto.price}`}</h4>
-        <Button />
-      </div>
+      <>
+        <div>
+          <h3 data-testid="product-detail-name">{ produto.title }</h3>
+          <img src={ produto.thumbnail } alt={ produto.title } />
+          <h4>{`R$ ${produto.price}`}</h4>
+          <Button />
+        </div>
+        <fieldset>
+          <label htmlFor="email">
+            <input type="text" id="email" name="email" required value={ email } onChange={ this.onChangeInput } />
+          </label>
+          <label htmlFor="textarea">
+            <textarea type="textarea" id="textarea" name="textarea" value={ textarea } onChange={ this.onChangeInput } />
+          </label>
+          <label htmlFor="nota">
+            <input type="radio" id="nota" data-testid="1-rating" name="nota" value={ nota } onChange={ this.onChangeInput } />
+            <input type="radio" id="nota" data-testid="2-rating" name="nota" value={ nota } onChange={ this.onChangeInput } />
+            <input type="radio" id="nota" data-testid="3-rating" name="nota" value={ nota } onChange={ this.onChangeInput } />
+            <input type="radio" id="nota" data-testid="4-rating" name="nota" value={ nota } onChange={ this.onChangeInput } />
+            <input type="radio" id="nota" data-testid="5-rating" name="nota" value={ nota } onChange={ this.onChangeInput } />
+          </label>
+          <button onClick={ this.click } type="submit">Avaliar</button>
+        </fieldset>
+      </>
     );
   }
 }
